41dbbab5f8b4e5ab18dc7e88db80624c0c0099bf
dpvpn09 <92319385+dpvpn09@users.noreply.github.com>
2021-10-18 13:50:01 +0700

    Update paket.sh

--- menu-all/paket.sh
+++ menu-all/paket.sh
@@ -4,43 +4,27 @@ GREEN='\e[0;32m'
 BLUE='\e[0;34m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-
 echo -e  "${RED}Checking VPS${NC}"
-
 sleep 2
-
 IZIN=$( curl https://raw.githubusercontent.com/dpvpn09/ipvps/main/ipvps | grep $MYIP )
-
 if [ $MYIP = $IZIN ]; then
-
 echo -e "${GREEN}Permission Accepted...${NC}"
-
 sleep 2 
-
 else
-
 clear
-
 echo -e ""
-
 echo -e "======================================="
-
 echo -e ""
-
 echo -e "${RED}Permission Denied...!!! ${NC}"
-
 echo -e "IP VPS ANDA BELUM TERDAFTAR"
-
-echo -e "Contact WA https//wa.me/+6281285970456"
-
+echo -e "Contact WA https//wa.me/+6282339191527"
 echo -e "For Registration IP VPS"
-
 echo -e ""
-
 echo -e "======================================="
-
 echo -e ""
-if
+exit 0
+fi
+
 clear
 echo "Start Update"
 # update
